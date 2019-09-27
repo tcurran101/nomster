148105c605a3859421451a3d785c3f10e6722055
Tiffany Curran <tcurran101@gmail.com>
2019-09-27 00:24:01 +0000

    aligned destroy method to user id

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -33,6 +33,9 @@ end
 
 def destroy
   @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
   @place.destroy
   redirect_to root_path
 end
