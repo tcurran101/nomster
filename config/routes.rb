9f782945af3a2f6ace9c07349de3db6ca6680198
Tiffany Curran <tcurran101@gmail.com>
2019-10-12 13:07:08 +0000

    added user controller and html pg

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   
-  resources :places
   resources :places do
       resources :comments, only: :create
       resources :photos, only: :create
     end
+  resources :users, only: :show
    end
