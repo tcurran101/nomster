74e1c603750ae7d44e2c3db6623da867a3e7a923
Tiffany Curran <tcurran101@gmail.com>
2019-10-13 15:06:06 +0000

    fixed migration files again. deleted line 4

--- db/migrate/20191001012829_alter_photos_add_user_id.rb
+++ db/migrate/20191001012829_alter_photos_add_user_id.rb
@@ -1,7 +1,6 @@
 class AlterPhotosAddUserId < ActiveRecord::Migration[5.2]
   def change
         add_column :photos, :picture, :string
-        add_column :photos, :user_id, :integer
-      end
+        end
     end
 
